9002d6f4eb336ceeec32c69d334a1dae8b49e592
Welton Carvalho <welton.carvalho.dev@gmail.com>
2026-08-03 21:21:31 -0300

    chore: initial commit

---
+++ core/src/main/java/dev/arcturuz/tic_tac_toe/ToeField.java
@@ -0,0 +1,35 @@
+package dev.arcturuz.tic_tac_toe;
+
+import com.badlogic.gdx.ApplicationListener;
+
+/**
+ * ToeField
+ */
+public class ToeField implements ApplicationListener{
+
+	@Override
+	public void create() {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void render() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+
+}
